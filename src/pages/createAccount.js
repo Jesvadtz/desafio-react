9fc7baf44f822080466d166c35a0c790ee4a82b9
jamesskips <roberto.garzah@udem.edu>
2022-03-26 19:58:38 -0600

    casi listo solo falta arreglar API

--- src/pages/createAccount.js
+++ src/pages/createAccount.js
@@ -8,33 +8,25 @@ function SignUp (){
    const [newUser, setNewUser] = React.useState({});
    const {name, lastName, nationality, biography, email, password} = newUser
 
-   function createUser(event) {
-     console.log(
+   const createUser = (event)=>{
     setNewUser({
       ...newUser,
-      [event.target.name]: event.target.value,
-    });
+      [event.target.name]: event.target.value
+        })
+        console.log(newUser)
   }
  
-  function enviarProducto(event){
+  async function enviarProducto(event){
       event.preventDefault()
       alert(JSON.stringify(newUser))
 
- /*     fetch('http://localhost:8080/writers/signUp',{
+    const res = await fetch('http://localhost:8080/writers/signUp',{
         method:"POST",
-        body:JSON.stringify(
-            {
-                name: name,
-                lastName: lastName,
-                nationality: nationality,
-                biography: biography,
-                email: email,
-                password:password
-            }
-        )
+        body:newUser,
     })
         .then(res=>res.json())
-        .then(json=>console.log(json))*/
+        .then(json=>console.log(json))
+        .catch(err => console.log(err))
   }
 
 
@@ -45,47 +37,47 @@ function SignUp (){
         <Form onSubmit={enviarProducto} style={{ display:"flex", flexDirection:"column", alignItems:"center", marginTop:"100px", marginBottom:"150px"}}>
         <Form.Group className="mb-3" style={{minWidth:"400px"}} controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Name" onChange={createUser} />
+          <Form.Control type="text" placeholder="Name" name="name" onChange={createUser} />
           <Form.Text className="text-muted">
             We'll never share your information with anyone.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" style={{minWidth:"400px"}} controlId="formBasicEmail">
           <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Last Name" onChange={createUser} />
+          <Form.Control type="text" placeholder="Last Name" name="lastName" onChange={createUser} />
           <Form.Text className="text-muted">
           We'll never share your information with anyone.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" style={{minWidth:"400px"}} controlId="formBasicEmail">
           <Form.Label>Nationality</Form.Label>
-          <Form.Control type="text" placeholder="Nationality" onChange={createUser} />
+          <Form.Control type="text" placeholder="Nationality" name="nationality" onChange={createUser} />
           <Form.Text className="text-muted">
           We'll never share your information with anyone.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" style={{minWidth:"400px"}} controlId="formBasicEmail">
           <Form.Label>Biography</Form.Label>
-          <Form.Control type="text" placeholder="Biography" onChange={createUser} />
+          <Form.Control type="text" placeholder="Biography" name="biography" onChange={createUser} />
           <Form.Text className="text-muted">
           We'll never share your information with anyone.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" style={{minWidth:"400px"}} controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="text" placeholder="Email" onChange={createUser} />
+          <Form.Control type="text" placeholder="Email" name="email" onChange={createUser} />
           <Form.Text className="text-muted">
           We'll never share your information with anyone.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" style={{minWidth:"400px"}} controlId="formBasicEmail">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="text" placeholder="Password" onChange={createUser} />
+          <Form.Control type="text" placeholder="Password" mail="password" onChange={createUser} />
           <Form.Text className="text-muted">
           We'll never share your information with anyone.
           </Form.Text>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onSubmit={enviarProducto}>
           Submit
         </Button>
         </Form>
