f1a4de35c9322fde038ffb495dd24703a01d56e7
RubS <rub.somuano.g@gmail.com>
2022-03-28 23:29:42 -0600

    merge/James

--- src/components/SideBarNavLink/SideBarNavLink.jsx
+++ src/components/SideBarNavLink/SideBarNavLink.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 
 function LeftBarNav(props) {
-    const {list} = props
-    return (
-        <ul>
-            {list.map((item) => {
-                return (
-                    <li>
-                        <a class="dropdown-item d-flex align-items-center" href="/">
-                            <img src={item.image} alt="home" style={{ width: "12%" }} />
-                            <h6 class="mb-0">&nbsp;&nbsp;{item.title}</h6>
-                        </a>
-                    </li>)
-            })}
-        </ul>
-    )
+  const { list, key } = props;
+  return (
+    <ul>
+      {list.map((item, index) => {
+        return (
+          <li key={index}>
+            <a className="dropdown-item d-flex align-items-center" href="/">
+              <img src={item.image} alt="home" style={{ width: "12%" }} />
+              <h6 className="mb-0" key={key}>
+                &nbsp;&nbsp;{item.title}
+              </h6>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default LeftBarNav;
--- src/components/hashTagList/hashTagList.jsx
+++ src/components/hashTagList/hashTagList.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import Tag from "../Tags/Tags";
 
 function HashtagList(props) {
-    const { hashtags } = props;
-    return (
-        <ul>
-            {
-                hashtags.map((item) => {
-                    return (
-                     <li>
-                        <Tag tag={item}/>
-                    </li>
-                    )
-                })
-            }
-        </ul>
-    )
+  const { hashtags, key } = props;
+  return (
+    <ul>
+      {hashtags.map((item, index) => {
+        return (
+          <li key={index}>
+            <Tag tag={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default HashtagList;
